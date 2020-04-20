5d6f60b47e981da7c32137c025604844eb92cc93
jake-bickle <bickle.jake@gmail.com>
2020-04-19 18:38:19 -0700

    Updates

---
+++ precache-manifest.c58ce7971989caff5b2ed80001f1301c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b49e4463e260affafe2671d2bc4aebe",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e72fb5cd69b1544f2526",
+    "url": "/react-tutorial/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e72fb5cd69b1544f2526",
+    "url": "/react-tutorial/static/js/2.42432f0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.42432f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484b1bd7bd8b274cd278",
+    "url": "/react-tutorial/static/js/main.6aa23a74.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
